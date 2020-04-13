932c0f4f653f913384b7b844aa10878d80b1621d
godswillokokon <godswillokokon3@gmail.com>
2020-04-13 12:19:41 +0100

    set custom theme

--- metro.config.js
+++ metro.config.js
@@ -4,8 +4,16 @@
  *
  * @format
  */
+const MetroConfig = require('@ui-kitten/metro-config');
 
-module.exports = {
+const evaConfig = {
+  evaPackage: '@eva-design/eva',
+  // Optional, but may be useful when using mapping customization feature.
+  // customMappingPath: './custom-mapping.json',
+};
+
+module.exports = MetroConfig.create(evaConfig, {
+  // Whatever was previously specified
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -14,4 +22,5 @@ module.exports = {
       },
     }),
   },
-};
+
+});
