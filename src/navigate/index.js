1fccbeebfbb81ac7df13c40783ca184dc0603b9e
godswillokokon <godswillokokon3@gmail.com>
2020-04-15 10:41:28 +0100

    set up slider nav

--- src/navigate/index.js
+++ src/navigate/index.js
@@ -1,11 +1,15 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 import { HomeScreen } from '../screens/home';
+import { LandingScreen, LandingTwoScreen, LandingThreeScreen } from '../screens/landing';
 // import { Bottom } from '../navigate/bottom';
 
 
 const HomeNavigator = createStackNavigator({
-  Home: HomeScreen,
+  // Home: HomeScreen,
+  Landing: LandingScreen,
+  LandingTwo: LandingTwoScreen,
+  LandingThree: LandingThreeScreen
   // Tab: AppNavigator,
 },
   {
