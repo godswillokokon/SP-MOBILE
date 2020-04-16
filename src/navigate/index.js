63c2f9a1bd928e5ca52312cd47324d7d0f97d085
godswillokokon <godswillokokon3@gmail.com>
2020-04-16 15:32:02 +0100

    signup nav setup

--- src/navigate/index.js
+++ src/navigate/index.js
@@ -1,17 +1,18 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
-import { HomeScreen } from '../screens/home';
 import { LandingScreen, LandingTwoScreen, LandingThreeScreen } from '../screens/landing';
 import { LoginScreen } from '../screens/login';
+import { SignupScreen } from '../screens/signup';
+import { HomeScreen } from '../screens/home';
 // import { Bottom } from '../navigate/bottom';
 
 
 const HomeNavigator = createStackNavigator({
-  // Home: HomeScreen,
   Landing: LandingScreen,
   LandingTwo: LandingTwoScreen,
   LandingThree: LandingThreeScreen,
-  Login: LoginScreen
+  Login: LoginScreen,
+  Signup: SignupScreen
   // Tab: AppNavigator,
 },
   {
