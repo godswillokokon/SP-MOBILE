b08e901a847551988af178fd4d0c3c7196c4427e
godswillokokon <godswillokokon3@gmail.com>
2020-04-16 08:03:57 +0100

    login nav setup

--- src/navigate/index.js
+++ src/navigate/index.js
@@ -2,6 +2,7 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 import { HomeScreen } from '../screens/home';
 import { LandingScreen, LandingTwoScreen, LandingThreeScreen } from '../screens/landing';
+import { LoginScreen } from '../screens/login';
 // import { Bottom } from '../navigate/bottom';
 
 
@@ -9,7 +10,8 @@ const HomeNavigator = createStackNavigator({
   // Home: HomeScreen,
   Landing: LandingScreen,
   LandingTwo: LandingTwoScreen,
-  LandingThree: LandingThreeScreen
+  LandingThree: LandingThreeScreen,
+  Login: LoginScreen
   // Tab: AppNavigator,
 },
   {
