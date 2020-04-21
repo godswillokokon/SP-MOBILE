88e1903720e05aba84e9304fc85980cb56870a49
godswillokokon <godswillokokon3@gmail.com>
2020-04-21 16:19:01 +0100

    top nav: comp

--- src/components/topNav.js
+++ src/components/topNav.js
@@ -1,26 +1,11 @@
-import React, { useState } from 'react'
-import {
-  StyleSheet,
-  View,
-  Dimensions,
-} from 'react-native';
-import { moderateScale } from 'react-native-size-matters';
+import React from 'react'
 import {
   TopNavigation,
 } from '@ui-kitten/components';
-import ToggleSwitch from 'toggle-switch-react-native';
 
-export default TopNav = ({ title, leftControl, rightControl }) => {
-
-
-
+export default TopNav = ({ LeftAction, RightAction, Title }) => {
   return (
-    <TopNavigation title={title} alignment={'center'}
-      accessoryLeft={leftControl} accessoryRight={rightControl} />
-
+    <TopNavigation title={Title} alignment={'center'}
+      accessoryLeft={LeftAction} accessoryRight={RightAction} />
   )
 };
-
-const styles = StyleSheet.create({
-
-})
