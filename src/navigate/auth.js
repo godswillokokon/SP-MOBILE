dde0fa4d1f2586633395f31c96d1443a926e4b2b
godswillokokon <godswillokokon3@gmail.com>
2020-04-30 23:58:05 +0100

    set up auth nav

--- src/navigate/auth.js
+++ src/navigate/auth.js
@@ -5,7 +5,6 @@ import { LoginScreen } from '../screens/login';
 import { SignupScreen } from '../screens/signup';
 import { ForgotPasswordScreen } from '../screens/forgotPassword';
 
-
 const Auth = createStackNavigator({
   Landing: LandingScreen,
   LandingTwo: LandingTwoScreen,
@@ -18,5 +17,3 @@ const Auth = createStackNavigator({
     headerMode: 'none',
   });
 export const AuthScreens = createAppContainer(Auth);
-
-
