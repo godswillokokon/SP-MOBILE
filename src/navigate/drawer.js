531a60639b2fcbad95defa4dcd8913268db33a55
godswillokokon <godswillokokon3@gmail.com>
2020-04-30 23:57:45 +0100

    set up darwer nav

--- src/navigate/drawer.js
+++ src/navigate/drawer.js
@@ -1,34 +1,43 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createAppContainer } from 'react-navigation';
+import { ScrollView } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-navigation';
+import { createDrawerNavigator } from 'react-navigation-drawer'
+
 import { DrawerContent } from '../components/drawerContent';
-
-import { HomeScreen } from '../screens/home';
 import { Bottom } from '../navigate/bottom';
-import { AuthScreens } from '../navigate/auth'
 
-
-
-const Drawer = createDrawerNavigator();
-
-
-const App = () => {
-  return (
-    <NavigationContainer>
-      {/* <RootStackScreen /> */}
-      {/* <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}> */}
-      <Drawer.Navigator>
-
-        {/* <Drawer.Screen name="HomeDrawer" component={HomeScreen} />
-        <Bottom /> */}
-        <Drawer.Screen name="Tab" component={Bottom} />
-        {/* <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
-        <Drawer.Screen name="BookmarkScreen" component={BookmarkScreen} /> */}
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
-}
-export const AppDrawer = createAppContainer(App);
-
-
+export const HamburgerNavigation = createDrawerNavigator(
+  {
+    Tabs: Bottom,
+  },
+  {
+    initialRouteName: 'Tabs',
+    contentComponent: props => {
+      return (
+        <ScrollView>
+          <SafeAreaView
+            forceInset={{ top: 'always', horizontal: 'never' }}
+          >
+            {/* <Text
+              onPress={() => {
+                props.navigation.navigate('Account');
+                props.navigation.closeDrawer();
+              }}
+            >
+              Account
+                    </Text>
+            <Text
+              onPress={() => {
+                props.navigation.navigate('Notification');
+                props.navigation.closeDrawer();
+              }}
+            >
+              Notification
+                    </Text> */}
+            <DrawerContent />
+          </SafeAreaView>
+        </ScrollView>
+      )
+    }
+  }
+);
