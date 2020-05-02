345599dfc8d20ba6c3ce16e4a525dab6a2f39b48
godswillokokon <godswillokokon3@gmail.com>
2020-05-02 04:28:29 +0100

    moved cats to screens

---
+++ src/screens/categories.js
@@ -0,0 +1,24 @@
+import SplashScreen from 'react-native-splash-screen';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+import {
+  Avatar, Card,
+} from '@ui-kitten/components';
+
+export const SampleScreen = () => {
+
+
+  return (
+    <View>
+      <Text>SampleScreen</Text>
+    </View>
+  )
+
+};
+
+const styles = StyleSheet.create({})
+
