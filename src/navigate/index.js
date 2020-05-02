70ce0ba8608a29061b99aa2b9e741da80e8916cf
godswillokokon <godswillokokon3@gmail.com>
2020-05-02 15:56:28 +0100

    nav: new app collection

--- src/navigate/index.js
+++ src/navigate/index.js
@@ -1,11 +1,13 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
-import { HamburgerNavigation } from '../navigate/drawer'
-import { AuthScreens } from '../navigate/auth'
+import { HamburgerNavigation } from '../navigate/drawer';
+import { AuthScreens } from '../navigate/auth';
+import { AppScreens } from '../navigate/application'
 
 const Stack = createStackNavigator({
   Auth: AuthScreens,
   Draw: HamburgerNavigation,
+  App: AppScreens
 
 },
   {
