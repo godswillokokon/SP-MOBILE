ed31aa6683c376bd160ae5e29ebc6ac402b94023
godswillokokon <godswillokokon3@gmail.com>
2020-05-02 04:29:19 +0100

    safearea view and overlay blur

--- src/screens/landing.js
+++ src/screens/landing.js
@@ -6,11 +6,9 @@ import {
   Text,
   ImageBackground,
   TouchableOpacity,
-  Dimensions
+  Dimensions,
+  SafeAreaView
 } from 'react-native';
-import {
-  Avatar, Card,
-} from '@ui-kitten/components';
 import LinearGradient from 'react-native-linear-gradient';
 
 export const LandingScreen = ({ navigation }) => {
@@ -39,50 +37,58 @@ export const LandingScreen = ({ navigation }) => {
   };
 
   return (
-    <ImageBackground source={require('../assets/one.png')} style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between' }}>
-      <View tyle={{ alignSelf: 'flex-start', }}>
-        <TouchableOpacity style={{ alignContent: 'space-around', width: 50, alignSelf: 'flex-end', margin: 15 }}>
-          <Text onPress={Skip} style={{
-            alignSelf: 'center', fontFamily: 'Muli', fontStyle: 'normal',
-            fontSize: 18,
-            color: '#fff',
-            textShadowColor: '#000',
-            fontWeight: 'bold'
-          }}>Skip</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={{ width: 300, margin: 15, position: 'absolute', top: 300, zIndex: 1 }}>
-        <Text style={{
-          fontSize: 38, color: '#fff',
-          lineHeight: 38, marginVertical: 5,
-        }}>Well Furnished Apartments</Text>
-        <Text style={{ marginVertical: 5, fontSize: 20, color: '#fff', lineHeight: 20 }}>
-          Lorem ipsum dolor sit amet, elit. Dictum consectetur adipiscing elit. Dictum
-        </Text>
-      </View>
-
-      <LinearGradient colors={['#00000100', '#00000159', '#00000175', '#00000191', '#000001b8', '#000001b8', '#000001b8', '#000001']} style={{ flex: 0.5 }} />
-      <View style={{
-        height: 40, backgroundColor: '#00959E', position: 'absolute', bottom: 0, width: Dimensions.get('window').width, flexDirection: 'row',
-      }}>
-        <View style={{ width: 80, alignItems: 'center', flexDirection: 'row', justifyContent: 'space-around', marginLeft: 10 }}>
-          <TouchableOpacity onPress={LandingOne}>
-            <View style={{ backgroundColor: '#CFCDCD', width: 10, height: 10, borderRadius: 30 }}></View>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={LandingTwo}>
-            <View style={{ backgroundColor: '#fff', width: 10, height: 10, borderRadius: 30 }}></View>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={LandingThree}>
-            <View style={{ backgroundColor: '#fff', width: 10, height: 10, borderRadius: 30 }}></View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ImageBackground source={require('../assets/one.png')} style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between' }}>
+        <View style={{
+          backgroundColor: 'rgba(0, 0, 0, 0.4)',
+          position: 'absolute',
+          width: Dimensions.get('window').width,
+          height: Dimensions.get('window').height,
+        }} />
+        <View tyle={{ alignSelf: 'flex-start', }}>
+          <TouchableOpacity style={{ alignContent: 'space-around', width: 50, alignSelf: 'flex-end', margin: 15 }}>
+            <Text onPress={Skip} style={{
+              alignSelf: 'center', fontFamily: 'Muli', fontStyle: 'normal',
+              fontSize: 18,
+              color: '#fff',
+              textShadowColor: '#000',
+              fontWeight: 'bold'
+            }}>Skip</Text>
           </TouchableOpacity>
         </View>
+        <View style={{ width: 300, margin: 15, position: 'absolute', top: 300, zIndex: 1 }}>
+          <Text style={{
+            fontSize: 42, color: '#fff',
+            lineHeight: 42, marginVertical: 5,
+          }}>Well Furnished Apartments</Text>
+          <Text style={{ marginVertical: 5, fontSize: 20, color: '#fff', lineHeight: 23 }}>
+            we always have you coverd, no stress. move into a well furnish and decorated house.
+        </Text>
+        </View>
 
-        <TouchableOpacity onPress={LandingTwo} style={{ position: 'absolute', right: 0, marginRight: 15, }}>
-          <Text style={{ marginTop: 7, fontSize: 18, color: '#fff', }}>Next</Text>
-        </TouchableOpacity>
+        <LinearGradient colors={['#00000100', '#00000159', '#00000175', '#00000191', '#000001b8', '#000001b8', '#000001b8', '#000001']} style={{ flex: 0.5 }} />
+        <View style={{
+          height: 40, backgroundColor: '#00959E', position: 'absolute', bottom: 0, width: Dimensions.get('window').width, flexDirection: 'row',
+        }}>
+          <View style={{ width: 80, alignItems: 'center', flexDirection: 'row', justifyContent: 'space-around', marginLeft: 10 }}>
+            <TouchableOpacity onPress={LandingOne}>
+              <View style={{ backgroundColor: '#CFCDCD', width: 10, height: 10, borderRadius: 30 }}></View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={LandingTwo}>
+              <View style={{ backgroundColor: '#fff', width: 10, height: 10, borderRadius: 30 }}></View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={LandingThree}>
+              <View style={{ backgroundColor: '#fff', width: 10, height: 10, borderRadius: 30 }}></View>
+            </TouchableOpacity>
+          </View>
 
-      </View>
-    </ImageBackground >
+          <TouchableOpacity onPress={LandingTwo} style={{ position: 'absolute', right: 0, marginRight: 15, }}>
+            <Text style={{ marginTop: 7, fontSize: 18, color: '#fff', }}>Next</Text>
+          </TouchableOpacity>
+
+        </View>
+      </ImageBackground >
+    </SafeAreaView>
   )
 
 };
@@ -110,50 +116,58 @@ export const LandingTwoScreen = ({ navigation }) => {
   };
 
   return (
-    <ImageBackground source={require('../assets/two.png')} style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between' }}>
-      <View tyle={{ alignSelf: 'flex-start', }}>
-        <TouchableOpacity onPress={Skip} style={{ alignContent: 'space-around', width: 50, alignSelf: 'flex-end', margin: 15 }}>
-          <Text style={{
-            alignSelf: 'center', fontFamily: 'Muli', fontStyle: 'normal',
-            fontSize: 18,
-            color: '#fff',
-            textShadowColor: '#000',
-            fontWeight: 'bold'
-          }}>Skip</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={{ width: 300, margin: 15, position: 'absolute', top: 300, zIndex: 1 }}>
-        <Text style={{
-          fontSize: 40, color: '#fff',
-          lineHeight: 38, marginVertical: 5,
-        }}>Well Furnished Apartments</Text>
-        <Text style={{ marginVertical: 5, fontSize: 20, color: '#fff', lineHeight: 20 }}>
-          Lorem ipsum dolor sit amet, elit. Dictum consectetur adipiscing elit. Dictum
-        </Text>
-      </View>
-
-      <LinearGradient colors={['#00000100', '#00000159', '#00000175', '#00000191', '#000001b8', '#000001b8', '#000001b8', '#000001']} style={{ flex: 0.5 }} />
-      <View style={{
-        height: 40, backgroundColor: '#00959E', position: 'absolute', bottom: 0, width: Dimensions.get('window').width, flexDirection: 'row',
-      }}>
-        <View style={{ width: 80, alignItems: 'center', flexDirection: 'row', justifyContent: 'space-around', marginLeft: 10 }}>
-          <TouchableOpacity onPress={LandingOne}>
-            <View style={{ backgroundColor: '#fff', width: 10, height: 10, borderRadius: 30 }}></View>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={LandingTwo}>
-            <View style={{ backgroundColor: '#CFCDCD', width: 10, height: 10, borderRadius: 30 }}></View>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={LandingThree}>
-            <View style={{ backgroundColor: '#fff', width: 10, height: 10, borderRadius: 30 }}></View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ImageBackground source={require('../assets/two.png')} style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between' }}>
+        <View style={{
+          backgroundColor: 'rgba(0, 0, 0, 0.4)',
+          position: 'absolute',
+          width: Dimensions.get('window').width,
+          height: Dimensions.get('window').height,
+        }} />
+        <View tyle={{ alignSelf: 'flex-start', }}>
+          <TouchableOpacity onPress={Skip} style={{ alignContent: 'space-around', width: 50, alignSelf: 'flex-end', margin: 15 }}>
+            <Text style={{
+              alignSelf: 'center', fontFamily: 'Muli', fontStyle: 'normal',
+              fontSize: 18,
+              color: '#fff',
+              textShadowColor: '#000',
+              fontWeight: 'bold'
+            }}>Skip</Text>
           </TouchableOpacity>
         </View>
+        <View style={{ width: 300, margin: 15, position: 'absolute', top: 300, zIndex: 1 }}>
+          <Text style={{
+            fontSize: 40, color: '#fff',
+            lineHeight: 42, marginVertical: 5,
+          }}>Affordable, to suit your budget</Text>
+          <Text style={{ marginVertical: 5, fontSize: 20, color: '#fff', lineHeight: 23 }}>
+            don't have enough money to purchase that dream home? let's save for it together with “spread save for property".
+        </Text>
+        </View>
 
-        <TouchableOpacity onPress={LandingThree} style={{ position: 'absolute', right: 0, marginRight: 15, }}>
-          <Text style={{ marginTop: 7, fontSize: 18, color: '#fff', }}>Next</Text>
-        </TouchableOpacity>
+        <LinearGradient colors={['#00000100', '#00000159', '#00000175', '#00000191', '#000001b8', '#000001b8', '#000001b8', '#000001']} style={{ flex: 0.5 }} />
+        <View style={{
+          height: 40, backgroundColor: '#00959E', position: 'absolute', bottom: 0, width: Dimensions.get('window').width, flexDirection: 'row',
+        }}>
+          <View style={{ width: 80, alignItems: 'center', flexDirection: 'row', justifyContent: 'space-around', marginLeft: 10 }}>
+            <TouchableOpacity onPress={LandingOne}>
+              <View style={{ backgroundColor: '#fff', width: 10, height: 10, borderRadius: 30 }}></View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={LandingTwo}>
+              <View style={{ backgroundColor: '#CFCDCD', width: 10, height: 10, borderRadius: 30 }}></View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={LandingThree}>
+              <View style={{ backgroundColor: '#fff', width: 10, height: 10, borderRadius: 30 }}></View>
+            </TouchableOpacity>
+          </View>
 
-      </View>
-    </ImageBackground >
+          <TouchableOpacity onPress={LandingThree} style={{ position: 'absolute', right: 0, marginRight: 15, }}>
+            <Text style={{ marginTop: 7, fontSize: 18, color: '#fff', }}>Next</Text>
+          </TouchableOpacity>
+
+        </View>
+      </ImageBackground >
+    </SafeAreaView>
   )
 
 };
@@ -187,51 +201,59 @@ export const LandingThreeScreen = ({ navigation }) => {
   };
 
   return (
-    <ImageBackground source={require('../assets/three.png')} style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between' }}>
-      <View tyle={{ alignSelf: 'flex-start', }}>
-        <TouchableOpacity onPress={Skip} style={{ alignContent: 'space-around', width: 50, alignSelf: 'flex-end', margin: 15 }}>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ImageBackground source={require('../assets/three.png')} style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between' }}>
+        <View style={{
+          backgroundColor: 'rgba(0, 0, 0, 0.4)',
+          position: 'absolute',
+          width: Dimensions.get('window').width,
+          height: Dimensions.get('window').height,
+        }} />
+        <View tyle={{ alignSelf: 'flex-start', }}>
+          <TouchableOpacity onPress={Skip} style={{ alignContent: 'space-around', width: 50, alignSelf: 'flex-end', margin: 15 }}>
+            <Text style={{
+              alignSelf: 'center', fontFamily: 'Muli', fontStyle: 'normal',
+              fontSize: 18,
+              color: '#fff',
+              textShadowColor: '#000',
+              fontWeight: 'bold'
+            }}>Skip</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{ width: 300, margin: 15, position: 'absolute', top: 300, zIndex: 1 }}>
           <Text style={{
-            alignSelf: 'center', fontFamily: 'Muli', fontStyle: 'normal',
-            fontSize: 18,
-            color: '#fff',
-            textShadowColor: '#000',
-            fontWeight: 'bold'
-          }}>Skip</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={{ width: 300, margin: 15, position: 'absolute', top: 300, zIndex: 1 }}>
-        <Text style={{
-          fontSize: 40, color: '#fff',
-          lineHeight: 38, marginVertical: 5,
-        }}>Well Furnished Apartments</Text>
-        <Text style={{ marginVertical: 5, fontSize: 20, color: '#fff', lineHeight: 20 }}>
-          Lorem ipsum dolor sit amet, elit. Dictum consectetur adipiscing elit. Dictum
+            fontSize: 40, color: '#fff',
+            lineHeight: 42, marginVertical: 5,
+          }}>Move easily from old to new houses</Text>
+          <Text style={{ marginVertical: 5, fontSize: 20, color: '#fff', lineHeight: 23 }}>
+            You no longer need to worry about moving all those heavy stuffs when relocating, we’ve got 24/7 reliable relocation team ready to serve you.
         </Text>
-      </View>
+        </View>
 
-      <LinearGradient colors={['#00000100', '#00000159', '#00000175', '#00000191', '#000001b8', '#000001b8', '#000001b8', '#000001']} style={{ flex: 0.5 }} />
-      <View style={{
-        height: 40, backgroundColor: '#00959E', position: 'absolute', bottom: 0, width: Dimensions.get('window').width, flexDirection: 'row',
-      }}>
-        <View style={{ width: 80, alignItems: 'center', flexDirection: 'row', justifyContent: 'space-around', marginLeft: 10 }}>
-          <TouchableOpacity onPress={LandingOne}>
-            <View style={{ backgroundColor: '#fff', width: 10, height: 10, borderRadius: 30 }}></View>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={LandingTwo}>
-            <View style={{ backgroundColor: '#fff', width: 10, height: 10, borderRadius: 30 }}></View>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={LandingThree}>
-            <View style={{ backgroundColor: '#CFCDCD', width: 10, height: 10, borderRadius: 30 }}></View>
+        <LinearGradient colors={['#00000100', '#00000159', '#00000175', '#00000191', '#000001b8', '#000001b8', '#000001b8', '#000001']} style={{ flex: 0.5 }} />
+        <View style={{
+          height: 40, backgroundColor: '#00959E', position: 'absolute', bottom: 0, width: Dimensions.get('window').width, flexDirection: 'row',
+        }}>
+          <View style={{ width: 80, alignItems: 'center', flexDirection: 'row', justifyContent: 'space-around', marginLeft: 10 }}>
+            <TouchableOpacity onPress={LandingOne}>
+              <View style={{ backgroundColor: '#fff', width: 10, height: 10, borderRadius: 30 }}></View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={LandingTwo}>
+              <View style={{ backgroundColor: '#fff', width: 10, height: 10, borderRadius: 30 }}></View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={LandingThree}>
+              <View style={{ backgroundColor: '#CFCDCD', width: 10, height: 10, borderRadius: 30 }}></View>
+            </TouchableOpacity>
+
+          </View>
+
+          <TouchableOpacity onPress={Login} style={{ position: 'absolute', right: 0, marginRight: 15, }}>
+            <Text style={{ marginTop: 7, fontSize: 18, color: '#fff', }}>Get Started</Text>
           </TouchableOpacity>
 
         </View>
-
-        <TouchableOpacity onPress={Login} style={{ position: 'absolute', right: 0, marginRight: 15, }}>
-          <Text style={{ marginTop: 7, fontSize: 18, color: '#fff', }}>Next</Text>
-        </TouchableOpacity>
-
-      </View>
-    </ImageBackground >
+      </ImageBackground >
+    </SafeAreaView>
   )
 
 };
