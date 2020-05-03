13436dea465428078a15c0428f080e46aedf7dbb
godswillokokon <godswillokokon3@gmail.com>
2020-05-03 11:17:41 +0100

    fixed eva config error

--- metro.config.js
+++ metro.config.js
@@ -6,11 +6,11 @@
  */
 const MetroConfig = require('@ui-kitten/metro-config');
 
-// const evaConfig = {
-//   evaPackage: '@eva-design/eva',
-//   // Optional, but may be useful when using mapping customization feature.
-//   // customMappingPath: './custom-mapping.json',
-// };
+const evaConfig = {
+  evaPackage: '@eva-design/eva',
+  // Optional, but may be useful when using mapping customization feature.
+  // customMappingPath: './custom-mapping.json',
+};
 
 module.exports = MetroConfig.create(evaConfig, {
   // Whatever was previously specified
