b065352c4629262ee4367309f04ed88eb819f1e2
godswillokokon <godswillokokon3@gmail.com>
2020-05-03 01:12:50 +0100

    theme

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import { ApplicationProvider } from '@ui-kitten/components';
 import { AppNavigatorScreens } from './src/navigate/index';
-// import { AppNavigator } from './src/navigate/navigator';
-import { default as appTheme } from './custom-theme.json';
+import { default as theme } from './custom-theme.json';
 import * as eva from '@eva-design/eva';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 
 
-const theme = { ...appTheme };
+// const theme = { ...appTheme };
 
 const App = () => (
   <React.Fragment>
-    <ApplicationProvider {...eva} theme={theme, eva.light}>
+    {/* <ApplicationProvider {...eva} theme={theme, eva.light}> */}
+    <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
       <NavigationContainer>
         <AppNavigatorScreens />
       </NavigationContainer>
-
     </ApplicationProvider>
   </React.Fragment>
 );
