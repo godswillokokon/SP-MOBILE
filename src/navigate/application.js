4ab4297517f3f7b65285477b99d3bf13131a0d38
godswillokokon <godswillokokon3@gmail.com>
2020-05-03 11:18:32 +0100

    added wallet nav at app

--- src/navigate/application.js
+++ src/navigate/application.js
@@ -1,10 +1,12 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 import { CategoriesScreen } from '../screens/categories';
+import { WalletScreen } from '../screens/wallet';
 
 
 const App = createStackNavigator({
   Categories: CategoriesScreen,
+  Wallet: WalletScreen
 
 },
   {
