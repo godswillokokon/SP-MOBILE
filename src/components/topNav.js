a138ca59cd20f786f5751650e34c86f59a2fb266
godswillokokon <godswillokokon3@gmail.com>
2020-05-05 15:31:17 +0100

    shadow style to top nav

--- src/components/topNav.js
+++ src/components/topNav.js
@@ -5,7 +5,18 @@ import {
 
 export default TopNav = ({ LeftAction, RightAction, Title }) => {
   return (
-    <TopNavigation title={Title} alignment={'center'}
+    <TopNavigation title={Title} alignment={'center'} style={{
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 0,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 0.09,
+
+      elevation: 2,
+      height: 10
+    }}
       accessoryLeft={LeftAction} accessoryRight={RightAction} />
   )
 };
