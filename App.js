7d995863082675d3d9e922264ff168a8403015fe
godswillokokon <godswillokokon3@gmail.com>
2020-06-06 03:40:27 +0100

    set store at APP.js

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { ApplicationProvider } from '@ui-kitten/components';
-import { AppNavigatorScreens } from './src/navigate/index';
-import { default as theme } from './custom-theme.json';
+import {ApplicationProvider} from '@ui-kitten/components';
+import {AppNavigatorScreens} from './src/navigate/index';
+import {default as theme} from './custom-theme.json';
 import * as eva from '@eva-design/eva';
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { Provider } from 'react-redux';
-import store from './src/redux/config/store'
+import {NavigationContainer} from '@react-navigation/native';
+import {allReducers} from './src/redux/reducers/allReducers';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 
+const store = createStore(allReducers, applyMiddleware(thunk));
 
 const App = () => (
   <Provider store={store}>
-    <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
+    <ApplicationProvider {...eva} theme={{...eva.light, ...theme}}>
       <NavigationContainer>
         <AppNavigatorScreens />
       </NavigationContainer>
