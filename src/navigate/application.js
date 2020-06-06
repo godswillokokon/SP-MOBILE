a31d08f15df12deeb3bff3da0e3c75e482ddcda5
godswillokokon <godswillokokon3@gmail.com>
2020-06-06 03:43:01 +0100

    nav fixes

--- src/navigate/application.js
+++ src/navigate/application.js
@@ -1,30 +1,29 @@
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
-import { CategoriesScreen } from '../screens/categories';
-import { WalletScreen } from '../screens/wallet';
-import { WalletTransationScreen } from '../screens/walletTransactions';
-import { ServicesScreen } from '../screens/services'
-import { ServiceScreen } from '../screens/service';
-import { ServiceBookingScreen } from '../screens/serviceBooking';
-import { PropertyScreen } from '../screens/property';
-import { AgentScreen } from '../screens/agent';
-import { SettingsScreen } from '../screens/settings';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
+import {CategoriesScreen} from '../screens/categories';
+import {WalletScreen} from '../screens/wallet';
+import {WalletTransationScreen} from '../screens/walletTransactions';
+import {ServicesScreen} from '../screens/services';
+import {ServiceScreen} from '../screens/service';
+import {ServiceBookingScreen} from '../screens/serviceBooking';
+import {PropertyScreen} from '../screens/property';
+import {AgentScreen} from '../screens/agent';
+import {SettingsScreen} from '../screens/settings';
 
-
-
-const App = createStackNavigator({
-  Categories: CategoriesScreen,
-  Wallet: WalletScreen,
-  WalletTransation: WalletTransationScreen,
-  Services: ServicesScreen,
-  Service: ServiceScreen,
-  ServiceBooking: ServiceBookingScreen,
-  Property: PropertyScreen,
-  Agent: AgentScreen,
-  Settings: SettingsScreen
-
-},
+const App = createStackNavigator(
+  {
+    Categories: CategoriesScreen,
+    Wallet: WalletScreen,
+    WalletTransation: WalletTransationScreen,
+    Services: ServicesScreen,
+    Service: ServiceScreen,
+    ServiceBooking: ServiceBookingScreen,
+    Property: PropertyScreen,
+    Agent: AgentScreen,
+    Settings: SettingsScreen,
+  },
   {
     headerMode: 'none',
-  });
+  },
+);
 export const AppScreens = createAppContainer(App);
--- src/navigate/index.js
+++ src/navigate/index.js
@@ -1,17 +1,18 @@
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
-import { HamburgerNavigation } from '../navigate/drawer';
-import { AuthScreens } from '../navigate/auth';
-import { AppScreens } from '../navigate/application'
+import {createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
+import {HamburgerNavigation} from '../navigate/drawer';
+import {AuthScreens} from '../navigate/auth';
+import {AppScreens} from '../navigate/application';
 
-const Stack = createStackNavigator({
-  Auth: AuthScreens,
-  App: AppScreens,
-  Draw: HamburgerNavigation,
-
-},
+const Stack = createStackNavigator(
+  {
+    Auth: AuthScreens,
+    App: AppScreens,
+    Draw: HamburgerNavigation,
+  },
   {
     headerMode: 'none',
-  });
+  },
+);
 
 export const AppNavigatorScreens = createAppContainer(Stack);
