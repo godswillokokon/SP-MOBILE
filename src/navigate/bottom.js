fc1c71837b8a3383afe783ad8c39e5ca83de422c
godswillokokon <godswillokokon3@gmail.com>
2020-06-07 01:18:43 +0100

    linting

--- src/navigate/bottom.js
+++ src/navigate/bottom.js
@@ -1,18 +1,17 @@
-import React from 'react'
-import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
-import { View } from 'react-native';
+import React from 'react';
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
+import {View} from 'react-native';
 import IconF from 'react-native-vector-icons/Feather';
 import IconO from 'react-native-vector-icons/Octicons';
 import IconM from 'react-native-vector-icons/MaterialCommunityIcons';
 import IconA from 'react-native-vector-icons/AntDesign';
 import IconI from 'react-native-vector-icons/Ionicons';
 //screens
-import { AccountScreen } from '../screens/account';
-import { HomeScreen } from '../screens/home';
-import { PropertiesScreen } from '../screens/properties';
-import { NotificationScreen } from '../screens/notification';
-import { SearchScreen } from '../screens/search';
-
+import {AccountScreen} from '../screens/account';
+import {HomeScreen} from '../screens/home';
+import {PropertiesScreen} from '../screens/properties';
+import {NotificationScreen} from '../screens/notification';
+import {SearchScreen} from '../screens/search';
 
 export const Bottom = createMaterialBottomTabNavigator(
   {
@@ -20,62 +19,74 @@ export const Bottom = createMaterialBottomTabNavigator(
       screen: HomeScreen,
       navigationOptions: {
         tabBarAccessibilityLabel: 'Home',
-        tabBarIcon: ({ tintColor }) => (
+        tabBarIcon: ({tintColor}) => (
           <View>
-            <IconO style={[{ color: tintColor }]} size={20} name={'home'} />
-          </View>),
+            <IconO style={[{color: tintColor}]} size={20} name={'home'} />
+          </View>
+        ),
       },
     },
     Properties: {
       screen: PropertiesScreen,
       navigationOptions: {
         tabBarAccessibilityLabel: 'Properties',
-        tabBarIcon: ({ tintColor }) => (
+        tabBarIcon: ({tintColor}) => (
           <View>
-            <IconM style={[{ color: tintColor }]} size={20} name={'home-city-outline'} />
-          </View>),
+            <IconM
+              style={[{color: tintColor}]}
+              size={20}
+              name={'home-city-outline'}
+            />
+          </View>
+        ),
       },
     },
     Search: {
       screen: SearchScreen,
       navigationOptions: {
         tabBarAccessibilityLabel: 'Search',
-        tabBarIcon: ({ tintColor }) => (
+        tabBarIcon: ({tintColor}) => (
           <View>
-            <IconA style={[{ color: tintColor }]} size={20} name={'search1'} />
-          </View>),
+            <IconA style={[{color: tintColor}]} size={20} name={'search1'} />
+          </View>
+        ),
       },
     },
     Notification: {
       screen: NotificationScreen,
       navigationOptions: {
         tabBarAccessibilityLabel: 'Notification',
-        tabBarIcon: ({ tintColor }) => (
+        tabBarIcon: ({tintColor}) => (
           <View>
-            <IconI style={[{ color: tintColor }]} size={20} name={'ios-notifications-outline'} />
-          </View>),
+            <IconI
+              style={[{color: tintColor}]}
+              size={20}
+              name={'ios-notifications-outline'}
+            />
+          </View>
+        ),
       },
     },
     Account: {
       screen: AccountScreen,
       navigationOptions: {
         tabBarAccessibilityLabel: 'Account',
-        tabBarIcon: ({ tintColor }) => (
+        tabBarIcon: ({tintColor}) => (
           <View>
-            <IconF style={[{ color: tintColor }]} size={20} name={'user'} />
-          </View>),
+            <IconF style={[{color: tintColor}]} size={20} name={'user'} />
+          </View>
+        ),
       },
     },
   },
   {
-    initialRouteName: "Home",
-    activeColor: "#00959E",
-    inactiveColor: "#8B95A6",
-    barStyle: { backgroundColor: '#fff', paddingVertical: 3 },
-    labelStyle: { fontSize: 12, },
+    initialRouteName: 'Home',
+    activeColor: '#00959E',
+    inactiveColor: '#8B95A6',
+    barStyle: {backgroundColor: '#fff', paddingVertical: 3},
+    labelStyle: {fontSize: 12},
     shifting: false,
     labeled: true,
     useLayoutAnimation: true,
-
-  }
+  },
 );
--- src/navigate/drawer.js
+++ src/navigate/drawer.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { ScrollView } from 'react-native-gesture-handler';
-import { SafeAreaView } from 'react-navigation';
-import { createDrawerNavigator } from 'react-navigation-drawer'
+import {ScrollView} from 'react-native-gesture-handler';
+import {SafeAreaView} from 'react-navigation';
+import {createDrawerNavigator} from 'react-navigation-drawer';
 
-import { DrawerContent } from '../components/drawerContent';
-import { Bottom } from '../navigate/bottom';
-
-
+import {DrawerContent} from '../components/drawerContent';
+import {Bottom} from '../navigate/bottom';
 
 export const HamburgerNavigation = createDrawerNavigator(
   {
@@ -15,14 +13,14 @@ export const HamburgerNavigation = createDrawerNavigator(
   {
     initialRouteName: 'Tabs',
     drawerType: 'slide',
-    contentComponent: props => {
+    contentComponent: (props) => {
       return (
         <ScrollView>
-          <SafeAreaView forceInset={{ top: 'always', horizontal: 'never' }}>
+          <SafeAreaView forceInset={{top: 'always', horizontal: 'never'}}>
             <DrawerContent props={props} />
           </SafeAreaView>
         </ScrollView>
-      )
-    }
-  }
+      );
+    },
+  },
 );
