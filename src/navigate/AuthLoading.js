b6c40ef4e1a9e2d8a75ce95d511b637a3d369e18
godswillokokon <godswillokokon3@gmail.com>
2020-06-07 01:18:10 +0100

    auth loading done

---
+++ src/navigate/AuthLoading.js
@@ -0,0 +1,28 @@
+import React, {useEffect} from 'react';
+import {ActivityIndicator, View, StyleSheet} from 'react-native';
+import Session from '@utils/Session';
+
+export const AuthLoadingScreen = ({navigation}) => {
+  useEffect(() => {
+    async function fetchToken() {
+      // Fetch the token from storage then navigate to our appropriate place
+      const userToken = await Session.getData('@token');
+      navigation.navigate(userToken ? 'Draw' : 'Auth');
+      // This will switch to the App screen or Auth screen and this loading
+      // screen will be unmounted and thrown away.
+      console.log(userToken, 'authhhh');
+    }
+    fetchToken();
+  }, [navigation]);
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color="#00959E" />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
--- src/navigate/auth.js
+++ src/navigate/auth.js
@@ -1,19 +1,25 @@
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
-import { LandingScreen, LandingTwoScreen, LandingThreeScreen } from '../screens/landing';
-import { LoginScreen } from '../screens/login';
-import { SignupScreen } from '../screens/signup';
-import { ForgotPasswordScreen } from '../screens/forgotPassword';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
+import {
+  LandingScreen,
+  LandingTwoScreen,
+  LandingThreeScreen,
+} from '../screens/landing';
+import {LoginScreen} from '../screens/login';
+import {SignupScreen} from '../screens/signup';
+import {ForgotPasswordScreen} from '../screens/forgotPassword';
 
-const Auth = createStackNavigator({
-  Landing: LandingScreen,
-  LandingTwo: LandingTwoScreen,
-  LandingThree: LandingThreeScreen,
-  Login: LoginScreen,
-  Signup: SignupScreen,
-  ForgotPassword: ForgotPasswordScreen,
-},
+const Auth = createStackNavigator(
+  {
+    Landing: LandingScreen,
+    LandingTwo: LandingTwoScreen,
+    LandingThree: LandingThreeScreen,
+    Login: LoginScreen,
+    Signup: SignupScreen,
+    ForgotPassword: ForgotPasswordScreen,
+  },
   {
     headerMode: 'none',
-  });
+  },
+);
 export const AuthScreens = createAppContainer(Auth);
