cd39b6b244e59653bdaa47d679331c1dc353a966
godswillokokon <godswillokokon3@gmail.com>
2020-06-07 01:18:50 +0100

    auth loading done

--- src/navigate/index.js
+++ src/navigate/index.js
@@ -3,15 +3,18 @@ import {createAppContainer} from 'react-navigation';
 import {HamburgerNavigation} from '../navigate/drawer';
 import {AuthScreens} from '../navigate/auth';
 import {AppScreens} from '../navigate/application';
+import {AuthLoadingScreen} from './AuthLoading';
 
 const Stack = createStackNavigator(
   {
+    AuthLoading: AuthLoadingScreen,
     Auth: AuthScreens,
     App: AppScreens,
     Draw: HamburgerNavigation,
   },
   {
     headerMode: 'none',
+    initialRouteName: 'AuthLoading',
   },
 );
 
