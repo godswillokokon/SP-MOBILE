42fea47c510563302ae19440a9e985d1fdf2f105
godswillokokon <godswillokokon3@gmail.com>
2020-06-11 14:22:40 +0100

    cleaned forgot password

--- src/screens/forgotPassword.js
+++ src/screens/forgotPassword.js
@@ -1,83 +1,89 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState} from 'react';
 import {
   StyleSheet,
   View,
   Text,
   Dimensions,
   KeyboardAvoidingView,
-  SafeAreaView
+  SafeAreaView,
+  TouchableOpacity,
 } from 'react-native';
-import {
-  TopNavigation,
-  TopNavigationAction,
-  Input
-} from '@ui-kitten/components';
+import {TopNavigation, TopNavigationAction, Input} from '@ui-kitten/components';
 import Icon from 'react-native-vector-icons/AntDesign';
 import IconF from 'react-native-vector-icons/FontAwesome5';
 import IconS from 'react-native-vector-icons/SimpleLineIcons';
 
-export const ForgotPasswordScreen = ({ navigation }) => {
+export const ForgotPasswordScreen = ({navigation}) => {
   const navigateBack = () => {
     requestAnimationFrame(() => {
       navigation.goBack();
-    })
+    });
   };
-
-  const BackIcon = () => (
-    <Icon style={[{ color: '#00959E', }]} name='arrowleft' size={25} />
-  );
-
+  const BackIcon = () => <Icon color={'#00959E'} name="arrowleft" size={25} />;
   const BackAction = () => (
-    <TopNavigationAction icon={BackIcon} onPress={navigateBack} style={[{ padding: 5 }]} />
+    <TopNavigationAction
+      icon={BackIcon}
+      onPress={navigateBack}
+      style={styles.left}
+    />
   );
   const Title = () => (
     <View>
-      <Text style={styles.title}>
-        Forgot Password
-    </Text>
+      <Text style={styles.title}>Forgot Password</Text>
     </View>
   );
   const MailIcon = () => (
     <View>
-      <IconF style={[{ color: '#828282' }]} size={18} name={'envelope'} />
+      <IconF color={'#828282'} size={18} name={'envelope'} />
     </View>
   );
-
-  const [Evalue, setValueE] = useState('');
+  const [email, setEmail] = useState('');
   return (
-    <SafeAreaView style={{ backgroundColor: '#fff', flex: 1 }}>
-      <TopNavigation title={Title} alignment={'center'} style={styles.topNavigation}
-        accessoryLeft={BackAction} />
-      <View style={{ alignSelf: 'center', margin: 50 }}>
-        <IconS style={[{ color: '#515C6F', }]} name='lock' size={50} />
+    <SafeAreaView style={styles.container}>
+      <TopNavigation
+        title={Title}
+        alignment={'center'}
+        style={styles.topNavigation}
+        accessoryLeft={BackAction}
+      />
+      <View style={styles.lock}>
+        <IconS color={'#515C6F'} name="lock" size={50} />
       </View>
-      <View style={{ alignSelf: 'center', width: Dimensions.get('window').width - 40, }}>
-        <Text style={{ fontSize: 15, color: '#3A3A3A', alignSelf: 'center', }}>If you forgot your password, please enter your</Text>
-        <Text style={{ fontSize: 15, color: '#3A3A3A', alignSelf: 'center' }}>email below and reset link would be sent to you</Text>
+      <View style={styles.textMain}>
+        <Text style={styles.paraOne}>
+          If you forgot your password, please enter your
+        </Text>
+        <Text style={styles.paraTwo}>
+          email below and reset link would be sent to you
+        </Text>
       </View>
       <KeyboardAvoidingView style={styles.key} behavior="padding" enabled>
         <Input
-          value={Evalue}
-          placeholder='Email'
+          value={email}
+          placeholder="Email"
           style={styles.inputEmail}
-          textStyle={styles.inputText}
-          labelStyle={styles.inputLabel}
-          captionTextStyle={styles.inputCaption}
-          onChangeText={setValueE}
+          onChangeText={setEmail}
           accessoryRight={MailIcon}
-          textStyle={styles.placeholder}
+          textStyle={styles.text}
           placeholderTextColor={'#828282'}
         />
       </KeyboardAvoidingView>
-
-      <View style={{ width: 98, height: 40, backgroundColor: '#00959E', alignSelf: 'center', borderRadius: 4, justifyContent: 'center', }}>
-        <Text style={{ alignSelf: 'center', fontSize: 18, color: '#fff' }}>Send</Text>
-      </View>
+      <TouchableOpacity style={styles.sendBtn}>
+        <Text style={styles.sendBtnText}>Send</Text>
+      </TouchableOpacity>
     </SafeAreaView>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    flex: 1,
+  },
+  lock: {
+    alignSelf: 'center',
+    margin: 50,
+  },
   title: {
     fontSize: 18,
     fontFamily: 'Muli',
@@ -85,8 +91,9 @@ const styles = StyleSheet.create({
     color: '#3A3A3A',
     fontWeight: 'bold',
   },
+  left: {padding: 5},
   topNavigation: {
-    shadowColor: "#000",
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
       height: 1,
@@ -98,17 +105,36 @@ const styles = StyleSheet.create({
     width: Dimensions.get('window').width + 5,
     flexDirection: 'row',
   },
-  inputEmail: {
-    borderColor: '#3A3A3A', backgroundColor: 'transparent',
-    width: Dimensions.get('window').width - 50, alignSelf: 'center',
-    margin: 40
-
+  textMain: {
+    alignSelf: 'center',
+    width: Dimensions.get('window').width - 40,
   },
-  inputText: { color: '#3A3A3A' },
-  placeholder: {
+  paraOne: {fontSize: 15, color: '#3A3A3A', alignSelf: 'center'},
+  paraTwo: {fontSize: 15, color: '#3A3A3A', alignSelf: 'center'},
+  inputEmail: {
+    borderColor: '#3A3A3A',
+    backgroundColor: 'transparent',
+    width: Dimensions.get('window').width - 50,
+    alignSelf: 'center',
+    margin: 40,
+  },
+  text: {
     fontSize: 13,
     fontFamily: 'Muli',
     fontWeight: 'bold',
-    color: '#3A3A3A'
+    color: '#3A3A3A',
   },
-})
+  sendBtn: {
+    width: 98,
+    height: 40,
+    backgroundColor: '#00959E',
+    alignSelf: 'center',
+    borderRadius: 4,
+    justifyContent: 'center',
+  },
+  sendBtnText: {
+    alignSelf: 'center',
+    fontSize: 18,
+    color: '#fff',
+  },
+});
