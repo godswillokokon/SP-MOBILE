dc7af73c914f4a8ff621d0b732409a361a1def84
godswillokokon <godswillokokon3@gmail.com>
2020-06-20 00:14:53 +0100

    update payment reducer

--- src/redux/reducers/allReducers.js
+++ src/redux/reducers/allReducers.js
@@ -2,9 +2,11 @@ import {combineReducers} from 'redux';
 import {counterReducer} from './counterReducer';
 import {userReducer} from './userReducer';
 import {propsReducer} from './propsReducer';
+import {paymentReducer} from './paymentsReducer';
 
 export const allReducers = combineReducers({
   counter: counterReducer,
   user: userReducer,
   properties: propsReducer,
+  payment: paymentReducer,
 });
