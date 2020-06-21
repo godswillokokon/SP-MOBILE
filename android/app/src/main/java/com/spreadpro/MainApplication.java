8f163ce541c9dc3dfa50c0938ffa2df6273763db
godswillokokon <godswillokokon3@gmail.com>
2020-06-21 13:13:12 +0100

    install video package

--- android/app/src/main/java/com/spreadpro/MainApplication.java
+++ android/app/src/main/java/com/spreadpro/MainApplication.java
@@ -3,6 +3,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.brentvatne.react.ReactVideoPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
