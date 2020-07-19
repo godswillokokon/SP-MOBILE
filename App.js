cfdaf7c7597c87c2310235a06029f32b243660f5
godswillokokon <godswillokokon3@gmail.com>
2020-07-20 00:54:52 +0100

    set up APP.js for Font Fam

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ApplicationProvider} from '@ui-kitten/components';
+import {ApplicationProvider, Layout, Button} from '@ui-kitten/components';
 import {AppNavigatorScreens} from './src/navigate/index';
 import {default as theme} from './custom-theme.json';
 import * as eva from '@eva-design/eva';
@@ -9,16 +9,32 @@ import {allReducers} from './src/redux/reducers/allReducers';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import {default as mapping} from './mapping.json';
+
+// const strictTheme = {['text-font-family']: 'OpenSans'}; // <-- Your Font
+// const customMapping = {strict: strictTheme};
 
 const store = createStore(allReducers, applyMiddleware(thunk));
 
+
 const App = () => (
   <Provider store={store}>
-    <ApplicationProvider {...eva} theme={{...eva.light, ...theme}}>
+    <ApplicationProvider
+      {...eva}
+      theme={{...eva.light, ...theme}}
+      mapping={mapping} >
       <NavigationContainer>
         <AppNavigatorScreens />
       </NavigationContainer>
     </ApplicationProvider>
   </Provider>
+  // <ApplicationProvider
+  //   {...eva}
+  //   theme={{...eva.dark, ...theme}}
+  //   customMapping={mapping}>
+  //   <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+  //     <Button>HOME</Button>
+  //   </Layout>
+  // </ApplicationProvider>
 );
 export default App;
