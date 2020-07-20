4c0a861f5c0da98e47fa8f1ac93446fa6509627a
godswillokokon <godswillokokon3@gmail.com>
2020-07-20 01:15:45 +0100

    touched props Image comp to suit api

--- src/components/propertyImages.js
+++ src/components/propertyImages.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import {
   View,
-  Text,
   Dimensions,
   TouchableOpacity,
   ImageBackground,
   StyleSheet,
+  ScrollView,
 } from 'react-native';
+import { Text } from '@ui-kitten/components';
 
-export default PropertyImages = ({
+import Image from 'react-native-transformable-image-next';
+const {width, height} = Dimensions.get('window');
+const PropertyImages = ({
   data,
   index,
   name,
@@ -19,22 +22,26 @@ export default PropertyImages = ({
 }) => {
   return (
     <TouchableOpacity onPress={() => onSelect(index)} style={styles.container}>
-      <ImageBackground
-        style={styles.imgBg}
-        source={{uri: data}}
-        // imageStyle={{ borderRadius: 6 }}
-      >
+      <ImageBackground style={styles.imgBg} source={{uri: data}}>
         <View style={styles.main}>
           <View style={styles.subMain}>
-            {/* <Text style={styles.name}>{name}</Text> */}
-            <Text style={styles.amount}>{amount}</Text>
+            <Text style={styles.name}>{name}</Text>
             <Text style={styles.address}>{address}</Text>
+            <Text style={styles.amount}>{amount}</Text>
           </View>
         </View>
       </ImageBackground>
+      {/* <Image
+        style={styles.imgBg}
+        source={{
+          uri: data,
+        }}
+        pixels={{width: 3607, height: 2400}}
+      /> */}
     </TouchableOpacity>
   );
 };
+export default PropertyImages;
 
 const styles = StyleSheet.create({
   container: {
@@ -48,6 +55,7 @@ const styles = StyleSheet.create({
   imgBg: {
     flex: 1,
     width: '100%',
+    // height: 200
   },
   main: {
     flex: 1,
