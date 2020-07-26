2ebb34086921659e55be834866d24606a0ab5537
godswillokokon <godswillokokon3@gmail.com>
2020-07-26 16:07:45 +0100

    reserved house actions

--- src/redux/actions/propsActions.js
+++ src/redux/actions/propsActions.js
@@ -1,8 +1,30 @@
 import * as axios from 'axios';
 import Session from '@utils/Session';
+import { ToastAndroid } from 'react-native';
 
+const showToast = (message) => {
+  ToastAndroid.show(message, ToastAndroid.LONG, ToastAndroid.TOP, 25, 50);
+};
 
 const BASE = 'https://api.spreadprolimited.com/api';
+
+const parseError = (err) => {
+  if (err?.response?.data?.errors) {
+    const message = Object.values(err.response.data.errors)[0];
+    console.log(message);
+  } else if (err?.response?.data?.error?.message) {
+    console.log(err.response.data.error.message);
+  } else if (err?.response?.data?.message) {
+    console.log(err.response.data.message);
+  } else if (err?.response?.data) {
+    console.log(err.response.data);
+  } else if (err.message) {
+    console.log(err.message);
+  } else {
+    console.log('Error Occured');
+  }
+};
+export default parseError;
 
 export const GetHouses = () => async (dispatch) => {
   try {
@@ -48,6 +70,37 @@ export const GetHouse = (slug) => async (dispatch) => {
       });
   } catch (error) {
     console.log(error, 'err');
+    dispatch({
+      type: 'FETCH_FAILED',
+      fetchError: error,
+    });
+  }
+};
+
+export const ReserveHouse = (slug) => async (dispatch) => {
+  try {
+    const token = await Session.getData('@token');
+    // console.log(slug);
+    await axios
+      .get(`https://api.spreadprolimited.com/api/house/${slug}/reserve`, {
+        headers: {
+          Authorization: token,
+          Accept: 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+      .then((response) => {
+        // console.log(response.data.property);
+        dispatch({
+          type: 'RESERVE_PROPERTY_SUCCESS',
+          payload: {
+            reserve: response.data.property,
+          },
+        });
+        showToast(response.data.message);
+      });
+  } catch (error) {
+    parseError(error);
     dispatch({
       type: 'FETCH_FAILED',
       fetchError: error,
