85f5cdb7d0aca20ddfc5692385ec64db327a3ca4
godswillokokon <godswillokokon3@gmail.com>
2020-08-09 22:35:36 +0100

    fund wallet Reducer done

--- src/redux/reducers/allReducers.js
+++ src/redux/reducers/allReducers.js
@@ -2,11 +2,13 @@ import {combineReducers} from 'redux';
 import {counterReducer} from './counterReducer';
 import {userReducer} from './userReducer';
 import {propsReducer} from './propsReducer';
-import {paymentReducer} from './paymentsReducer';
+// import {paymentReducer} from './paymentsReducer';
+import {walletReducer} from './walletReducer';
 
 export const allReducers = combineReducers({
   counter: counterReducer,
   user: userReducer,
   properties: propsReducer,
+  wallet: walletReducer,
   // payment: paymentReducer,
 });
