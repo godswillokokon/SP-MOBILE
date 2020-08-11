888ca944a58e76a0f34f5621b9f07d455bd5b472
godswillokokon <godswillokokon3@gmail.com>
2020-08-12 00:03:11 +0100

    careers list done

--- src/screens/services.js
+++ src/screens/services.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import {
   StyleSheet,
   View,
@@ -6,111 +6,74 @@ import {
   FlatList,
   TouchableOpacity,
   SafeAreaView,
-  Image
+  Image,
+  BackHandler,
 } from 'react-native';
-import {
-  TopNavigationAction, Text,
-} from '@ui-kitten/components';
+import {TopNavigationAction, Text} from '@ui-kitten/components';
 import TopNav from '../components/topNav';
 import IconA from 'react-native-vector-icons/AntDesign';
+import PropertiesPlaceholder from '../components/propertiesStaging';
 
+import {useSelector, useDispatch} from 'react-redux';
+import {GetCareers} from '../redux/actions/careersActions';
 
+export const ServicesScreen = ({navigation}) => {
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      navigateBack,
+    );
+    dispatch(GetCareers());
 
+    return () => backHandler.remove();
+  }, [dispatch, navigateBack]);
+  const {careers} = useSelector((state) => state.careers);
 
-export const ServicesScreen = ({ navigation }) => {
-
-
-
-  const DATA_Categories = [
-    {
-      id: '1',
-      title: 'Housing agent',
-      subTitle: 'Need help in searching for your dream home?',
-      imageUrl: 'https://res.cloudinary.com/ogcodes/image/upload/v1588506184/SP/agent.png',
-      details: {
-        title: 'Agents',
-        info: 'Are you about to hire a Construction Services Provider? But you are not sure what offerings they have to make your home or office space more comfortable and luxurious? we are reliable, contact us today to undertake all your building projects for you at affordable prices. We offer timely and top notch services which will make your buildings stand out amongst the rest. Distance is not a barrier; we are just a phone call away. Do not hesitate to award us your building contracts.',
-        imageUrl: 'https://res.cloudinary.com/ogcodes/image/upload/v1588506184/SP/agent.png',
-        rate: 2000
-      }
-
-    },
-    {
-      id: '2',
-      title: 'Movers',
-      subTitle: 'Ready to relocate and need help moving?',
-      imageUrl: 'https://res.cloudinary.com/ogcodes/image/upload/v1588507566/SP/mover.png',
-      details: {
-        title: 'Movers',
-        info: 'Are you about to hire a Construction Services Provider? But you are not sure what offerings they have to make your home or office space more comfortable and luxurious? we are reliable, contact us today to undertake all your building projects for you at affordable prices. We offer timely and top notch services which will make your buildings stand out amongst the rest. Distance is not a barrier; we are just a phone call away. Do not hesitate to award us your building contracts.',
-        imageUrl: 'https://res.cloudinary.com/ogcodes/image/upload/v1588507566/SP/mover.png',
-        rate: 2200
-      }
-
-    },
-    {
-      id: '3',
-      title: 'Construction',
-      subTitle: 'Need a building plan? lets be your plug',
-      imageUrl: 'https://res.cloudinary.com/ogcodes/image/upload/v1588506184/SP/consrt.png',
-      details: {
-        title: 'Contractors',
-        info: 'Are you about to hire a Construction Services Provider? But you are not sure what offerings they have to make your home or office space more comfortable and luxurious? we are reliable, contact us today to undertake all your building projects for you at affordable prices. We offer timely and top notch services which will make your buildings stand out amongst the rest. Distance is not a barrier; we are just a phone call away. Do not hesitate to award us your building contracts.',
-        imageUrl: 'https://res.cloudinary.com/ogcodes/image/upload/v1588506184/SP/consrt.png',
-        rate: 68000
-      }
-    },
-    {
-      id: '4',
-      title: 'Plumbering',
-      subTitle: 'Need help in searching for your dream home?',
-      imageUrl: 'https://res.cloudinary.com/ogcodes/image/upload/v1588506184/SP/plumber.png',
-      details: {
-        title: 'Plumbers',
-        info: 'Are you about to hire a Construction Services Provider? But you are not sure what offerings they have to make your home or office space more comfortable and luxurious? we are reliable, contact us today to undertake all your building projects for you at affordable prices. We offer timely and top notch services which will make your buildings stand out amongst the rest. Distance is not a barrier; we are just a phone call away. Do not hesitate to award us your building contracts.',
-        imageUrl: 'https://res.cloudinary.com/ogcodes/image/upload/v1588506184/SP/plumber.png',
-        rate: 1520
-      }
-    },
-  ];
   //top nav
   const navigateBack = () => {
     requestAnimationFrame(() => {
       navigation.navigate('Hamburger');
-
-    })
+    });
   };
   const navigateService = (data) => {
     requestAnimationFrame(() => {
-
       navigation.navigate('Service', {
-        details: data
+        details: data,
       });
-
-    })
+    });
   };
   const Left = () => (
-    <IconA style={[{ color: '#00959E', }]} name='arrowleft' size={25} />
+    <IconA style={[{color: '#00959E'}]} name="arrowleft" size={25} />
   );
 
   const LeftAction = () => (
-    <TopNavigationAction icon={Left} onPress={navigateBack} style={[{ padding: 5 }]} />
+    <TopNavigationAction
+      icon={Left}
+      onPress={navigateBack}
+      style={[{padding: 5}]}
+    />
   );
   const Title = () => (
-    <View >
-      <Text style={styles.title}>
-        Services
-      </Text>
-    </View >
+    <View>
+      <Text style={styles.title}>Careers</Text>
+    </View>
   );
 
-  function Categories({ id, title, subTitle, imageUrl, selected, onSelect, details }) {
-    // console.log(details, "oo")
+  function Categories({
+    id,
+    name,
+    description,
+    image,
+    selected,
+    onSelect,
+    approved_experts,
+  }) {
+    // console.log(approved_experts, "oo")
     return (
-
       <TouchableOpacity
-        onPress={() => navigateService(details)}
+        onPress={() => navigateService(approved_experts)}
         style={{
           marginVertical: 12,
           marginHorizontal: 5,
@@ -118,7 +81,7 @@ export const ServicesScreen = ({ navigation }) => {
           height: 126,
           borderRadius: 4,
           backgroundColor: '#fff',
-          shadowColor: "#000",
+          shadowColor: '#000',
           shadowOffset: {
             width: 0,
             height: 2,
@@ -126,27 +89,45 @@ export const ServicesScreen = ({ navigation }) => {
           shadowOpacity: 0.25,
           shadowRadius: 3.84,
           elevation: 5,
-
-        }}
-      >
-        <View style={{ flex: 1, width: 100, height: 100, borderRadius: 4, alignSelf: 'center' }}>
-          <Image style={{ flex: 1, width: 100, height: 100 }}
-            source={{ uri: imageUrl }}
-
-          />
-          <View style={{
-            flex: 1, backgroundColor: '#fff', flexDirection: 'column',
-            justifyContent: 'flex-end', borderRadius: 6,
+        }}>
+        <View
+          style={{
+            flex: 1,
+            width: 100,
+            height: 100,
+            borderRadius: 4,
+            alignSelf: 'center',
           }}>
-            <View style={{ margin: 5 }}>
-              <Text style={{
-                fontSize: 12,
-                color: '#0DABA8', lineHeight: 13
-              }}>{title}</Text>
-              <Text style={{
-                fontSize: 10,
-                color: '#828282', lineHeight: 14
-              }}>{subTitle}</Text>
+          <Image
+            style={{flex: 1, width: 100, height: 100}}
+            source={{uri: image}}
+          />
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: '#fff',
+              flexDirection: 'column',
+              justifyContent: 'flex-end',
+              borderRadius: 6,
+            }}>
+            <View style={{margin: 5}}>
+              <Text
+                style={{
+                  fontSize: 12,
+                  color: '#0DABA8',
+                  lineHeight: 13,
+                }}>
+                {name}
+              </Text>
+              <Text
+                style={{
+                  fontSize: 10,
+                  color: '#828282',
+                  lineHeight: 14,
+                  maxHeight: 50,
+                }}>
+                {description}
+              </Text>
             </View>
           </View>
         </View>
@@ -158,7 +139,7 @@ export const ServicesScreen = ({ navigation }) => {
   const [selected, setSelected] = useState(new Map());
 
   const onSelect = useCallback(
-    id => {
+    (id) => {
       const newSelected = new Map(selected);
       newSelected.set(id, !selected.get(id));
       setSelected(newSelected);
@@ -167,31 +148,29 @@ export const ServicesScreen = ({ navigation }) => {
   );
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{flex: 1}}>
       <TopNav Title={Title} LeftAction={LeftAction} />
-      <View style={{ flex: 1, alignSelf: 'center', marginVertical: 24, }}>
+      <View style={{flex: 1, alignSelf: 'center', marginVertical: 24}}>
         <FlatList
-          data={DATA_Categories}
-          renderItem={({ item }) => (
+          data={careers}
+          renderItem={({item}) => (
             <Categories
               id={item.id}
-              title={item.title}
-              subTitle={item.subTitle}
-              imageUrl={item.imageUrl}
-              details={item.details}
+              name={item.name}
+              description={item.description}
+              image={item.image}
+              approved_experts={item.approved_experts}
               selected={!!selected.get(item.id)}
               onSelect={onSelect}
             />
           )}
-          keyExtractor={item => item.id}
+          keyExtractor={(item) => item.id}
           extraData={selected}
           numColumns={3}
         />
       </View>
-
-    </SafeAreaView >
-  )
-
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -202,4 +181,4 @@ const styles = StyleSheet.create({
     color: '#3A3A3A',
     fontWeight: 'bold',
   },
-})
+});
