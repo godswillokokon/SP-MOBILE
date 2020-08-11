4d139a8eb0594170e115a64213eb570657e87c91
godswillokokon <godswillokokon3@gmail.com>
2020-08-12 00:05:12 +0100

    careers list reducer

--- src/redux/reducers/allReducers.js
+++ src/redux/reducers/allReducers.js
@@ -1,14 +1,12 @@
 import {combineReducers} from 'redux';
-import {counterReducer} from './counterReducer';
 import {userReducer} from './userReducer';
 import {propsReducer} from './propsReducer';
-// import {paymentReducer} from './paymentsReducer';
+import {careersReducer} from './careersReducer';
 import {walletReducer} from './walletReducer';
 
 export const allReducers = combineReducers({
-  counter: counterReducer,
   user: userReducer,
   properties: propsReducer,
   wallet: walletReducer,
-  // payment: paymentReducer,
+  careers: careersReducer,
 });
